254fb7bbd5d56203dc16cbfea66700a1dda96416
Brandon Sprague <brandon00sprague@gmail.com>
2023-02-14 11:37:27 -0800

    Attempt to add a new Tag endpoint

--- registry/tags.go
+++ registry/tags.go
@@ -1,5 +1,11 @@
 package registry
 
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
 type tagsResponse struct {
 	Tags []string `json:"tags"`
 }
@@ -22,4 +28,48 @@ func (registry *Registry) Tags(repository string) (tags []string, err error) {
 			return nil, err
 		}
 	}
+}
+
+type tagsFullResponse struct {
+	Count    int       `json:"count"`
+	Next     *string   `json:"next"`
+	Previous *string   `json:"previous"`
+	Results  []TagFull `json:"results"`
+}
+
+type TagFull struct {
+	Creator int64          `json:"creator"`
+	ID      int64          `json:"id"`
+	Images  []TagFullImage `json:"images"`
+}
+
+type TagFullImage struct {
+	Architecture string    `json:"architecture"`
+	Features     string    `json:"features"`
+	Variant      *string   `json:"variant"`
+	Digest       string    `json:"digest"`
+	OS           string    `json:"os"`
+	OSFeatures   string    `json:"os_features"`
+	OSVersion    *string   `json:"os_version"`
+	Size         int64     `json:"size"`
+	Status       string    `json:"status"`
+	LastPulled   time.Time `json:"last_pulled"`
+	LastPushed   time.Time `json:"last_pushed"`
+}
+
+func (registry *Registry) TagsFull(repository string) ([]TagFull, error) {
+	url := registry.url("/v2/repositories/%s/tags", repository)
+
+	resp, err := registry.Client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error calling repository tag endpoint: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var tagsResp tagsFullResponse
+	if json.NewDecoder(resp.Body).Decode(&tagsResp); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %w", err)
+	}
+
+	return tagsResp.Results, nil
 }
